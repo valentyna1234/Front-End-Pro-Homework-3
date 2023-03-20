c1521bc5760f00b1c7625d283bb28818b797ef92
valentyna1234 <pisareva.valya@gmail.com>
2023-03-20 17:31:25 +0200

    Add index.js file

---
+++ index.js
@@ -0,0 +1,30 @@
+//Отримання значень від користувача
+let a, b;
+a = Number(prompt( 'Enter the first value, please.' ));
+console.log(a);
+b = Number(prompt( 'Enter the second value, please.' ));
+console.log(b);
+
+//Додавання
+let c = a + b;
+console.log(c);
+
+//Віднімання
+let d = a - b;
+console.log(d);
+
+//Множення
+let e = a * b;
+console.log(e);
+
+//Ділення
+let f = a / b;
+console.log(f);
+
+//Виведення отриманих даних
+alert( a + '+' + b + '=' + c + "\n" + a + '-' + b + '=' + d + "\n" + a + '*' + b + '=' + e +"\n" + a + '/' + b + '=' + f )
+
+
+
+
+
